fc0066c9055b096e95988925554c32c7d3001cfe
asemin08 <semin.allan@gmail.com>
2022-02-21 15:12:33 +0100

    Ajout WSDL récupération

--- src/eu/ensup/allan/Hello.java
+++ src/eu/ensup/allan/Hello.java
@@ -4,7 +4,7 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
 @WebService
-@SOAPBinding(style = SOAPBinding.Style.RPC)
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public interface Hello {
     String ditBonjour(String name);
 }
