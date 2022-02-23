350a843463f4ff22dff555717884362810739c94
asemin08 <semin.allan@gmail.com>
2022-02-24 00:13:30 +0100

    Ajout Message dans le header + Modif pour différencier les enveloppes entrante et sortante

--- src/eu/ensup/webservices/allan/interceptor/SoapMessageHandler.java
+++ src/eu/ensup/webservices/allan/interceptor/SoapMessageHandler.java
@@ -1,18 +1,19 @@
 package eu.ensup.webservices.allan.interceptor;
 
 import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.*;
+import javax.xml.ws.handler.Handler;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.awt.*;
 import java.io.IOException;
-import java.util.Set;
+import java.util.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SoapMessageHandler implements SOAPHandler<SOAPMessageContext> {
-
 
     @Override
     public Set<QName> getHeaders() {
@@ -21,11 +22,33 @@ public class SoapMessageHandler implements SOAPHandler<SOAPMessageContext> {
 
     @Override
     public boolean handleMessage(SOAPMessageContext context) {
-        SOAPMessage msg = context.getMessage();
+        try{
+            SOAPMessage msg = context.getMessage();
+            SOAPPart part =  msg.getSOAPPart();
+            SOAPEnvelope envelope = part.getEnvelope();
+            SOAPHeader header = envelope.getHeader();
 
-        try {
+            //Ajout valeur au header
+            SOAPElement el = header.addChildElement(envelope.createName("user", "toto","*"));
+            el.setValue("UserTest");
+
+            msg.saveChanges();
+            Boolean isOutMessage = (Boolean) context.get (MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+            // Différencier le message d'entrée et de sortie
+            if (isOutMessage.booleanValue()) {
+
+                System.out.println("\nmessage sortie:");
+            } else {
+                System.out.println("\nmessage d'entree:");
+            }
+            System.out.println("\nReponse : "+ msg);
+
+            //Lire le context du message
             msg.writeTo(System.out);
-        } catch (SOAPException | IOException e) {
+
+
+        } catch (Exception e) {
             Logger.getLogger(SoapMessageHandler.class.getName()).log(Level.SEVERE, null, e);
         }
         return true;
@@ -40,4 +63,8 @@ public class SoapMessageHandler implements SOAPHandler<SOAPMessageContext> {
     public void close(MessageContext context) {
 
     }
+
+
+
+
 }
